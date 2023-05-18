f7ab82b116df0f6392d5d226ba88d2f47007ded3
Андрей <88152811+moonmann@users.noreply.github.com>
2023-05-18 12:57:20 +0300

    7 laba

---
+++ AOIS_lab7.cpp
@@ -0,0 +1,17 @@
+﻿#include "Proc.h"
+
+int main()
+{
+    Memory m;
+    m.getRandomMass();
+    cout << m << endl;
+    bool valueFind;
+    cout << "1 - sort Descending, 0 - Ascending" << endl;
+    cin >> valueFind;
+    if (valueFind) m.sortDescending();
+    else  m.sortAscending();
+    cout << m << endl;
+    string testValue = "1XX0XXX0";
+    Memory findTestValue = m.findByBool(testValue);
+    cout << findTestValue;
+}
